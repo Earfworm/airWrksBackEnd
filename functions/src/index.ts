be66733170cce24340d054fff049964e53a1f0f6
earfworm <deshawnreid2021@gmail.com>
2023-03-27 17:10:57 -0400

    commenting and code review

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import contactAirwrksRouter from "./routes/ContactAirWrksRouter";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -8,8 +9,8 @@ app.use(express.json());
 // app.post("/contactAirWrks", async (req, res) => {
 //   console.log(req);
 // });
+
 app.use("/contactAirWrks", contactAirwrksRouter);
 
-app.listen(4000, () =>
-  console.log(`Images microservice listening on port ${4000}!`)
-);
+//
+app.listen(4000, () => console.log(`listening on port ${4000}!`));
